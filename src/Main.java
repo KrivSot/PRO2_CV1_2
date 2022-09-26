433dab8aeb8301a2fa83eb3667a59df32d031c43
krivsot <krys396@seznam.cz>
2022-09-26 11:27:19 +0200

    1.01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import models.Box;
 import models.ExampleClass;
 import models.MyMathClass;
+import models.Pair;
 
 public class Main {
 
@@ -15,5 +17,10 @@ public class Main {
         //MyMathClass aa = new MyMathClass();
 
         System.out.print(MyMathClass.InnerClass.x);
+        Box<ExampleClass> box = new Box<>(exCls);
+        Box<String> boxString = new Box<>("Ahoj");
+        Box<Integer> boxInt = new Box<>(2);
+        Pair<Integer, String> myPair = new Pair<>(1, "Praha");
+        Pair<Integer, String> myPair2 = new Pair<>(2, "Hradec K.");
     }
 }
